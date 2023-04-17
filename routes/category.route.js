46db970d5eccbcd89f6d9e4730cfdfd257c55699
Abhishek <abhi22sen2000@gmail.com>
2023-04-17 10:39:46 +0530

    token

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { removeCategory, saveCategory , categoryList} from '../controller/category.controller.js';
+import { verifyToken } from '../middleware/verification.js';
 
 const router=express.Router();
 
-router.post('/saveCategory',saveCategory);
-router.post('/removeCategory',removeCategory);
+router.post('/saveCategory',verifyToken,saveCategory);
+router.post('/removeCategory',verifyToken,removeCategory);
 router.get("/categoryList",categoryList);
 
 export default router;
